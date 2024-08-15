cf6295c0b4d421f6386d14f0e9a48db3727e1afb
bhabna01 <abierhoque01@gmail.com>
2024-08-15 23:01:54 +0600

    Service Area added

---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+
+const Services = () => {
+    const [services, setServices] = useState([])
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    })
+    return (
+        <div className="mt-4">
+            <div className="text-center">
+                <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
+                <h2 className="text-5xl">Our Service Area</h2>
+                <p>Experience pure elegance with treatments that are carefully crafted,<br></br> free from random trends and filled with genuine care and expertise—because your beauty deserves nothing less. </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
+                    ></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
