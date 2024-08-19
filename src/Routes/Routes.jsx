8d7db0949acb0f87403b9f4af1423f51df1096de
bhabna01 <abierhoque01@gmail.com>
2024-08-19 23:53:49 +0600

    Private Route added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,10 @@ import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import Signup from "../pages/Signup/Signup";
 
+import PrivateRoute from "./PrivateRoute";
+import BookService from "../pages/BookService/BookService";
+import Bookings from "../pages/Bookings/Bookings";
+
 const router = createBrowserRouter([
     {
         path: "/",
@@ -20,6 +24,17 @@ const router = createBrowserRouter([
             {
                 path: "/signup",
                 element: <Signup></Signup>
+            },
+            {
+                path: "/book/:id",
+                element: <PrivateRoute><BookService></BookService></PrivateRoute>,
+                loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+            }
+            ,
+            {
+                path: "/bookings",
+                element: <PrivateRoute><Bookings></Bookings></PrivateRoute>,
+
             }
         ]
     },
