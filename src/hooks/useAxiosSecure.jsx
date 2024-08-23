a858931e3ad90842c5457a4f34164e1d9857a5db
bhabna01 <abierhoque01@gmail.com>
2024-08-23 20:39:54 +0600

    Custom hook added as well as JWT functionalites

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+export const axiosSecure = axios.create({
+    baseURL: 'http://localhost:5000',
+    withCredentials: true
+})
+const useAxiosSecure = () => {
+    const { logOut } = useAuth()
+    const navigate = useNavigate()
+    useEffect(() => {
+        axiosSecure.interceptors.response.use(res => {
+            return res;
+        }, error => {
+            console.log("Error tracked in the interceptor", error.response)
+            if (error.response.status === 401 || error.response.status === 403) {
+                console.log("Logout the user")
+                logOut()
+                    .then(() => {
+                        navigate("/login")
+                    })
+                    .catch(error => console.log(error))
+            }
+
+
+        })
+    }, [])
+    return axiosSecure
+};
+
+export default useAxiosSecure;
